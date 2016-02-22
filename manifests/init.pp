7787d53228fa06bd7242aefa264f5676eaa24607
gobunatarajan <gobu.natarajan@gmail.com>
2016-02-22 17:21:00 +0530

    Tested in CentOS, Ubuntu and Debian
    Fixed issues with various flavors like CentOS, Ubuntu and Debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,14 +4,16 @@
 class gsutil(
 # Derives the Installation Directory
   $install_dir = 'UNSET'
-)
-  {
-  include gsutil::params
+) inherits gsutil::params {
 
   $install_dir_final = $install_dir ? {
     'UNSET' => $::gsutil::params::install_dir,
     default => $install_dir,
 }
+
+  notify {"FileName : $download_filename":}
+  notify {"Install : $install_dir":}
+  notify {"Source : $download_source":}
 
 # Validates if the installation directory path exists
   validate_absolute_path($install_dir_final)
@@ -20,15 +22,17 @@ class gsutil(
   File { owner => 'root', group => 'root', mode => '0666', }
 
 # The below block of code downloads the gsutil archive file.
-  archive::download { $::download_filename:
-    url      => $::download_source,
+
+    archive::download { $download_filename:
+    url => $download_source,
     checksum => false,
+  
   }
 
 # The below block of code extracts the gsutil archive file.
   archive::extract { 'gsutil':
     target  => $install_dir_final,
-    require => Archive::Download[$::download_filename],
+    require => Archive::Download[$download_filename],
   }
 
 # The below block of code installs the gsutil archive file.
